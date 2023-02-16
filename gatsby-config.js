0d8fd628d744f7e890f28d449a0541f13798f855
Xiyue Zhang <xiyuej.zhang@mail.utoronto.ca>
2023-02-15 21:31:16 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Jasmine Zhang`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
